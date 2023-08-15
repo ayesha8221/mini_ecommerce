14341b9baafa0940474135f92194e7c5427c8627
ayesha8221 <ayesha.glnt012@gmail.com>
2023-08-15 16:35:30 +0200

    backend

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "bz7euxaqesnez3zdbxlo-mysql.services.clever-cloud.com",
+  user: "uviblsf9ix1jtt6d",
+  password: "OcXPpKaFcw4T82BSTAb9",
+  database : "bz7euxaqesnez3zdbxlo",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("select * from products", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
+
+export default con;
